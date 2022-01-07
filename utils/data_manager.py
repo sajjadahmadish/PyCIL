57efd67ec30eb033706f5fb327e11dbcabfbc6a0
Sajjad Ahmadi <sajjad.ahmadi.sh@gmail.com>
2022-01-07 15:40:37 +0330

    Update data_manager.py

--- utils/data_manager.py
+++ utils/data_manager.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
-from utils.data import iCIFAR10, iCIFAR100, iImageNet100, iImageNet1000
+from utils.data import iCIFAR10, iCIFAR100, iImageNet100, iImageNet1000, iModelNet40
 
 
 class DataManager(object):
@@ -172,6 +172,8 @@ def _get_idata(dataset_name):
         return iImageNet1000()
     elif name == "imagenet100":
         return iImageNet100()
+    elif name == "pointnet":
+        return iModelNet40()
     else:
         raise NotImplementedError('Unknown dataset {}.'.format(dataset_name))
 
